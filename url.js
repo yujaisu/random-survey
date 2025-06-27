938957f2a1c7f85410a9b80009d862a69a998509
yujaisu <petty891102mouse@gmail.com>
2025-06-28 06:05:34 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/QErMTdkZ8bikBehw6',
+	'https://forms.gle/HnRzmR3GfWJJtFfU7',
 ];
